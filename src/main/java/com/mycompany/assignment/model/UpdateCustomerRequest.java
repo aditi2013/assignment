ca3c722218e98de571dacfdf140d36de7ad60ae8
jaydip <jaydip10@gmail.com>
2025-02-25 16:36:21 +0530

    Assignment-1: Add validations on create and update customer requests

--- src/main/java/com/mycompany/assignment/model/UpdateCustomerRequest.java
+++ src/main/java/com/mycompany/assignment/model/UpdateCustomerRequest.java
@@ -1,17 +1,23 @@
 package com.mycompany.assignment.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-public class UpdateCustomerRequest {
+@Getter
+public class UpdateCustomerRequest extends CreateCustomerRequest {
+    @NotNull
     private final UUID id;
-    private final String firstName;
-    private final String middleName;
-    private final String lastName;
-    private final String phone;
-    private final String email;
+
+    public UpdateCustomerRequest(UUID id, String firstName, String middleName,
+                                 String lastName, String phone, String email) {
+        super(firstName, middleName, lastName, phone, email);
+        this.id = id;
+    }
 }
